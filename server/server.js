e388f3750ccf108e32defed82b9484dede14eecf
beardedguru <charles@thebeardedguru.com>
2018-01-30 18:08:16 -0500

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -19,14 +19,14 @@ app.post('/todos', (req, res) => {
     text: req.body.text,
   });
 
-  todo.save().then(
-    doc => {
+  todo
+    .save()
+    .then(doc => {
       res.send(doc);
-    },
-    e => {
+    })
+    .catch(e => {
       res.status(400).send(e);
-    }
-  );
+    });
 });
 
 app.get('/todos', (req, res) => {
@@ -112,6 +112,24 @@ app.patch('/todos/:id', (req, res) => {
     })
     .catch(e => {
       res.status(400).send();
+    });
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken();
+    })
+    .then(token => {
+      res.header('x-auth', token).send(user);
+    })
+    .catch(e => {
+      res.status(400).send(e);
     });
 });
 
